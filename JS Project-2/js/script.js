de9d65b8ef5ed85a849d24c57626b58b119ff728
McCauleyWomble <123336224+McCauleyWomble@users.noreply.github.com>
2023-02-12 13:42:59 -0700

    Java proj 1

---
+++ JS Project-2/js/script.js
@@ -0,0 +1,7 @@
+var firstName = "McCauley";
+var lastName = "Bullock Womble";
+var age = 18;
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("I'm having a blast learning about HTML and JavaScript in this class!");
